30b5143fca9f932cc9e14546e9b363e72e093251
ReshmaJS <you@example.com>
2026-02-16 17:23:58 +0530

    lint fixed

--- producer/nsselection_for_pdu_session.go
+++ producer/nsselection_for_pdu_session.go
@@ -16,7 +16,6 @@ import (
 	"math/rand"
 	"net/http"
 
-	"github.com/omec-project/nssf/logger"
 	"github.com/omec-project/nssf/plugin"
 	"github.com/omec-project/nssf/util"
 	"github.com/omec-project/openapi/models"
@@ -33,25 +32,15 @@ func selectNsiInformation(nsiInformationList []models.NsiInformation) models.Nsi
 
 // Network slice selection for PDU session
 // The function is executed when the IE, `slice-info-for-pdu-session`, is provided in query parameters
-func nsselectionForPduSession(
-	param plugin.NsselectionQueryParameter,
+func nsselectionForPduSession(param plugin.NsselectionQueryParameter,
 	authorizedNetworkSliceInfo *models.AuthorizedNetworkSliceInfo,
 	problemDetails *models.ProblemDetails,
 ) int {
-
-	logger.NssfLog.Infof("Entered nsselectionForPduSession")
-
 	var status int
-
 	if param.HomePlmnId != nil {
-		logger.NssfLog.Infof("HomePlmnId provided")
-
+		// Check whether UE's Home PLMN is supported when UE is a roamer
 		if !util.CheckSupportedHplmn(*param.HomePlmnId) {
-			logger.NssfLog.Warnf("Unsupported Home PLMN")
-
-			authorizedNetworkSliceInfo.RejectedNssaiInPlmn =
-				append(authorizedNetworkSliceInfo.RejectedNssaiInPlmn,
-					*param.SliceInfoRequestForPduSession.SNssai)
+			authorizedNetworkSliceInfo.RejectedNssaiInPlmn = append(authorizedNetworkSliceInfo.RejectedNssaiInPlmn, *param.SliceInfoRequestForPduSession.SNssai)
 
 			*problemDetails = models.ProblemDetails{
 				Title:  util.UNSUPPORTED_RESOURCE,
@@ -59,36 +48,26 @@ func nsselectionForPduSession(
 				Detail: "Home PLMN is not supported",
 				Cause:  "SNSSAI_NOT_SUPPORTED",
 			}
-
 			status = http.StatusForbidden
-			logger.NssfLog.Infof("Returning status: %d", status)
 			return status
 		}
 	}
 
 	if param.Tai != nil {
-		logger.NssfLog.Infof("TAI provided")
-
+		// Check whether UE's current TA is supported when UE provides TAI
 		if !util.CheckSupportedTa(*param.Tai) {
-			logger.NssfLog.Warnf("Unsupported TA")
-
-			authorizedNetworkSliceInfo.RejectedNssaiInTa =
-				append(authorizedNetworkSliceInfo.RejectedNssaiInTa,
-					*param.SliceInfoRequestForPduSession.SNssai)
+			authorizedNetworkSliceInfo.RejectedNssaiInTa = append(authorizedNetworkSliceInfo.RejectedNssaiInTa, *param.SliceInfoRequestForPduSession.SNssai)
 
 			status = http.StatusOK
-			logger.NssfLog.Infof("Returning status: %d", status)
 			return status
 		}
 	}
 
 	if param.Tai != nil &&
-		!util.CheckSupportedSnssaiInPlmn(
-			*param.SliceInfoRequestForPduSession.SNssai,
-			*param.Tai.PlmnId) {
-
-		logger.NssfLog.Warnf("S-NSSAI not supported in PLMN")
-
+		!util.CheckSupportedSnssaiInPlmn(*param.SliceInfoRequestForPduSession.SNssai, *param.Tai.PlmnId) {
+		// Return ProblemDetails indicating S-NSSAI is not supported
+		// TODO: Based on TS 23.501 V15.2.0, if the Requested NSSAI includes an S-NSSAI that is not valid in the
+		//       Serving PLMN, the NSSF may derive the Configured NSSAI for Serving PLMN
 		*problemDetails = models.ProblemDetails{
 			Title:  util.UNSUPPORTED_RESOURCE,
 			Status: http.StatusForbidden,
@@ -97,18 +76,12 @@ func nsselectionForPduSession(
 		}
 
 		status = http.StatusForbidden
-		logger.NssfLog.Infof("Returning status: %d", status)
 		return status
 	}
 
 	if param.HomePlmnId != nil {
-		if param.SliceInfoRequestForPduSession.RoamingIndication ==
-			models.RoamingIndication_NON_ROAMING {
-
-			logger.NssfLog.Warnf("Contradiction: home-plmn-id + NON_ROAMING")
-
+		if param.SliceInfoRequestForPduSession.RoamingIndication == models.RoamingIndication_NON_ROAMING {
 			problemDetail := "`home-plmn-id` is provided, which contradicts `roamingIndication`:'NON_ROAMING'"
-
 			*problemDetails = models.ProblemDetails{
 				Title:  util.INVALID_REQUEST,
 				Status: http.StatusBadRequest,
@@ -122,19 +95,12 @@ func nsselectionForPduSession(
 			}
 
 			status = http.StatusBadRequest
-			logger.NssfLog.Infof("Returning status: %d", status)
 			return status
 		}
 	} else {
-		if param.SliceInfoRequestForPduSession.RoamingIndication !=
-			models.RoamingIndication_NON_ROAMING {
-
-			logger.NssfLog.Warnf("Contradiction: roamingIndication without home-plmn-id")
-
-			problemDetail := fmt.Sprintf(
-				"`home-plmn-id` is not provided, which contradicts `roamingIndication`:'%s'",
+		if param.SliceInfoRequestForPduSession.RoamingIndication != models.RoamingIndication_NON_ROAMING {
+			problemDetail := fmt.Sprintf("`home-plmn-id` is not provided, which contradicts `roamingIndication`:'%s'",
 				string(param.SliceInfoRequestForPduSession.RoamingIndication))
-
 			*problemDetails = models.ProblemDetails{
 				Title:  util.INVALID_REQUEST,
 				Status: http.StatusBadRequest,
@@ -148,43 +114,27 @@ func nsselectionForPduSession(
 			}
 
 			status = http.StatusBadRequest
-			logger.NssfLog.Infof("Returning status: %d", status)
 			return status
 		}
 	}
 
-	if param.Tai != nil &&
-		!util.CheckSupportedSnssaiInTa(
-			*param.SliceInfoRequestForPduSession.SNssai,
-			*param.Tai) {
-
-		logger.NssfLog.Warnf("S-NSSAI not supported in TA")
-
-		authorizedNetworkSliceInfo.RejectedNssaiInTa =
-			append(authorizedNetworkSliceInfo.RejectedNssaiInTa,
-				*param.SliceInfoRequestForPduSession.SNssai)
-
+	if param.Tai != nil && !util.CheckSupportedSnssaiInTa(*param.SliceInfoRequestForPduSession.SNssai, *param.Tai) {
+		// Requested S-NSSAI does not supported in UE's current TA
+		// Add it to Rejected NSSAI in TA
+		authorizedNetworkSliceInfo.RejectedNssaiInTa = append(authorizedNetworkSliceInfo.RejectedNssaiInTa, *param.SliceInfoRequestForPduSession.SNssai)
 		status = http.StatusOK
-		logger.NssfLog.Infof("Returning status: %d", status)
 		return status
 	}
 
-	logger.NssfLog.Infof("Fetching NSI Information from config")
-
-	nsiInformationList :=
-		util.GetNsiInformationListFromConfig(
-			*param.SliceInfoRequestForPduSession.SNssai)
+	nsiInformationList := util.GetNsiInformationListFromConfig(*param.SliceInfoRequestForPduSession.SNssai)
 
 	if nsiInformationList == nil {
-		logger.NssfLog.Warnf("No NSI Information found")
 		*authorizedNetworkSliceInfo = models.AuthorizedNetworkSliceInfo{}
 	} else {
-		logger.NssfLog.Infof("Selecting NSI Information")
 		nsiInformation := selectNsiInformation(nsiInformationList)
 		authorizedNetworkSliceInfo.NsiInformation = new(models.NsiInformation)
 		*authorizedNetworkSliceInfo.NsiInformation = nsiInformation
 	}
 
-	logger.NssfLog.Infof("Returning status: %d", http.StatusOK)
 	return http.StatusOK
 }
